897f5687fb1836d422f708c395f4ef6c252d9988
Balint Erdosi <erdosib@gmail.com>
2019-12-24 01:28:20 +0100

    Use metric prop in Forecast, make metricRepresentation use it.
    Signed-off-by: Balint Erdosi <erdosib@gmail.com>

--- js/ForecastCityForecastPanel.js
+++ js/ForecastCityForecastPanel.js
@@ -6,7 +6,8 @@ var weatherAppGlobal = weatherAppGlobal || {};
 	Vue.component("forecast-city-forecast-panel", {
 		props: {
 			forecastItems: { type: Array, default: [] },
-			toShow: { type: Boolean, default: false }
+			toShow: { type: Boolean, default: false },
+			metric: { type: String, default: "metric" }
 		},
 		mixins: [WeatherApp.mixins.hasMetricRepresentation],
 		template: "#forecast-forecast-panel-template"
